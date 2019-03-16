2c83eb820decbf23a774bcfc700c219b8ec9f13f
Peter Gordon <superwebdeveloper@gmail.com>
2019-03-15 22:22:58 -0700

    sample tbs4 jumbotron layout to kick things off

---
+++ laravel/resources/views/hello.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.jumbo')
+
+@section('content')
+<div class="container">
+    <h1 class="display-3">Hello template</h1>
+    <p> first template, trying things out<p>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+    </div>
+@endsection
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/hello', function () {
+    return view('hello');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
