4c179604503920008e525910c0cc3701c06ae9da
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2019-03-29 01:34:50 -0700

    feature test for topics now submits data

--- laravel/database/factories/TopicFactory.php
+++ laravel/database/factories/TopicFactory.php
@@ -21,14 +21,14 @@ $factory->define(Topic::class, function (Faker $faker) {
     $slug = strtolower($title);
 
     return [
-        'topic.name' => $title,
-        'topic.slug' => $slug,
-        'topic.description' => $faker->text(50),
-        'topic.image' => strtolower($faker->text(5)).'.jpg',
-        'topic.scope' => 'public',
-        'topic.live' => 'yes',
-        'topic.sort_order' => 1000,
-        'topic.in_menu' => 'yes',
-        'topic.allow_comments' => 'yes',
+        'name' => $title,
+        'slug' => $slug,
+        'description' => $faker->text(50),
+        'image' => strtolower($faker->text(5)).'.jpg',
+        'scope' => 'public',
+        'live' => 'yes',
+        'sort_order' => '1000',
+        'in_menu' => 'yes',
+        'allow_comments' => 'yes',
         ];
 });
--- laravel/tests/Feature/TopicTest.php
+++ laravel/tests/Feature/TopicTest.php
@@ -7,7 +7,7 @@ use Tests\TestCase;
 use App\Models\Topic;
 use Illuminate\Http\Response;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 
@@ -22,54 +22,50 @@ class TopicTest extends TestCase
     public function testBasicTest()
     {
 
-        $response = $this->get('/admin/topic/');
+/*        $response = $this->get('/admin/topic/');
 
         if ($response->assertStatus(Response::HTTP_OK)) {
             $response->assertSeeText("Topics");
-            echo "\n Admin create topics page \n";
-        }
+        }*/
 
-        $topics = factory(Topic::class, 10)->make();
+        echo "\n Admin create topics page \n";
+
+        $topics = factory(Topic::class, 20)->make();
 
         foreach ($topics as $topic) {
 
             echo "attempting to insert ". $topic['name'] . "\n";
 
-            $response = $this->get('/admin/topic/');
-
-            $response = $this->json(
-                'POST',
+            $response = $this->post(
                 '/admin/topic',
                 [
-                    'topic.name' => $topic['name'],
-                    'topic.description' => $topic['description'],
-                    'topic.image' => $topic['image'],
-                    'topic.scope' => $topic['scope'],
-                    'topic.live' => $topic['live'],
-                    'topic.in_menu' => $topic['in_menu'],
-                    'topic.allow_comments' => $topic['allow_comments'],
+                    'topic'=>[
+                        'name' => $topic['name'],
+                        'description' => $topic['description'],
+                        'image' => $topic['image'],
+                        'scope' => $topic['scope'],
+                        'live' => $topic['live'],
+                        'sort_order' => $topic['sort_order'],
+                        'in_menu' => $topic['in_menu'],
+                        'allow_comments' => $topic['allow_comments'],
+                    ],
                     '_token' => Session::token(),
                 ]
             );
 
-            // not submitting yet
-
             echo  $topic['name'] . " has been posted. \n";
 
-            $response = $this->get('/admin/topic');
-            sleep(1);
-
-            if ($response->assertStatus(Response::HTTP_OK)) {
+/*            if ($response->assertStatus(Response::HTTP_OK)) {
                 echo $topic['name'] . " has been stored in db. \n";
-            }
+            }*/
 
         }
 
-        $response = $this->get('/admin/topics');
+/*        $response = $this->get(route('topics_list'));
 
         if ($response->assertStatus(Response::HTTP_OK)) {
             $response->assertSeeText("List Topics");
             echo "\n Admin List topics page \n";
-        }
+        }*/
     }
 }
