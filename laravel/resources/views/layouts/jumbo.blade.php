2d5e620ae684cb0ffcf284ddc0b7c96eb9caf814
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2019-04-13 15:39:55 -0700

    links in nav bar

--- laravel/resources/views/layouts/jumbo.blade.php
+++ laravel/resources/views/layouts/jumbo.blade.php
@@ -52,14 +52,14 @@
     <body>
     <a name="top"></a>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">{{config('app.name')}}</a>
+        <a class="navbar-brand" href="{{ route('hello') }}">{{config('app.name')}}</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="{{ route('hello') }}">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('contact')}}">Contact Us</a>
