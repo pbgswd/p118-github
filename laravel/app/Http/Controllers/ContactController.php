7d9815384e5c695f0a5bc57961d3aa4c0593a448
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2019-06-15 23:32:50 -0700

    contact controller

--- laravel/app/Http/Controllers/ContactController.php
+++ laravel/app/Http/Controllers/ContactController.php
@@ -31,9 +31,19 @@ class ContactController extends Controller
      */
     public function submit(SubmitContact $request)
     {
+
+        dump($request->all());
+      //  echo $_ENV['ADMIN_EMAIL'];
+dump(env('ADMIN_EMAIL'));
+dump(env('ADMIN_EMAIL_NAME'));
+dump(env('APP_NAME'));
+dump(config('ADMIN_EMAIL'));
+        dd(config('app.env'));
+        exit();
+
         Mail::send('emails.contact', ['data'=>$request->all()], function ($m) use ($request) {
             $m->from($request['email'], $request['name']);
-            $m->to(env('ADMIN_EMAIL_RECIPIENT'), env('ADMIN_EMAIL_NAME'))->subject('Contact Page ' . $request['subject']);
+            $m->to(env('ADMIN_EMAIL'), env('ADMIN_EMAIL_NAME'))->subject('Contact Page ' . $request['subject']);
         });
 
         flash()->success('Your message was sent.');
