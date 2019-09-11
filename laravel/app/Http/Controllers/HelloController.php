ceb28469ff58bdf6ca1bf4dfedf49a7aa22c1949
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2019-09-11 16:55:02 -0700

    composer updates, for Carbon. Also, nice date message on birthday on home page, hello controller, hello blade

--- laravel/app/Http/Controllers/HelloController.php
+++ laravel/app/Http/Controllers/HelloController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Hello;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HelloController extends Controller
@@ -14,9 +15,14 @@ class HelloController extends Controller
      */
     public function index(\App\Models\Hello $data)
     {
-        //
-	$data['foundingYear'] = 1904;
-	$data['years'] = date('Y') - $data['foundingYear'];
+        $data['foundingYear'] = 1904;
+        $today = Carbon::today();
+        $data['foundingDate'] = Carbon::createMidnightDate(1904, 9, 13);
+        $data['years'] = $data['foundingDate']->diffInYears($today);
+        if ($today->isBirthday($data['foundingDate']))
+        {
+             $data['birthday'] = "Happy Birthday IATSE Local 118! You are " . $data['years'] . " years young today!";
+        }
 
         return view('hello', ['data'=>$data]);
     }
--- laravel/resources/views/hello.blade.php
+++ laravel/resources/views/hello.blade.php
@@ -6,15 +6,19 @@
             <h1>{{config('app.name')}}</h1>
         </div>
         <div class="col-12">
+            @if($data['birthday'] != '')
+                <h2> {{ $data['birthday'] }}</h2>
+            @endif
+
             <h3>{{$data['years']}} years, since {{$data['foundingYear']}}.</h3>
         </div>
         <div class="col-12">
-            <p><span class="dropcap">F</span>ounded in 1904,
+            <p><span class="dropcap">F</span>ounded on September 13, 1904,
                 IATSE Local 118 (International Alliance of Theatrical Stage Employees of the United States and Canada)
                 is the labour union supplying technicians, stagehands, artisans and craftspeople to the Greater Vancouver
                 entertainment industry, including live theatre, rock and roll, trade shows, and conventions.
                 Local 118 has a large, skilled and experienced workforce ready to meet the needs of your production. </p>
-            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+            <p><a class="btn btn-primary btn-lg" href="/page/about-us" role="button">Learn more &raquo;</a></p>
         </div>
     </div>
 </div>
