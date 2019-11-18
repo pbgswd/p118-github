c7fd86a2881b172dd383b5ca6cbbcf0717bb820f
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2019-11-17 21:07:43 -0800

    checking in Committee MVC and migration and request validators

---
+++ laravel/database/migrations/2019_11_18_002749_create_committees_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommitteesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('committees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->string('description')->nullable();
+            $table->string('access_level')->nullable();
+            $table->boolean('live');
+            $table->integer('sort_order')->unsigned()->nullable();
+            $table->boolean('in_menu');
+            $table->boolean('allow_comments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('committees');
+    }
+}
