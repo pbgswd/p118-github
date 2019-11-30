adb7572bbc33e87355d9d33c75b0d211e6b060d9
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2019-11-29 22:16:09 -0800

    with timestamps in eloquent relationship. some more output in blade. Migration to fix committee user pivot table

---
+++ laravel/database/migrations/2019_11_29_225554_update_commttee_user.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateCommtteeUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // add role
+        Schema::table('users_committees_pivot', function (Blueprint $table) {
+            // rename to committee_user
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::rename('users_committees_pivot',  'committee_user');
+    }
+
+    /**
+     * Reverse the migration.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //drop role
+        Schema::table('committee_user', function (Blueprint $table) {
+
+            $table->dropColumn('created_at');
+            $table->dropColumn('updated_at');
+            $table->dropColumn('deleted_at');
+            // rename to users_committees_pivot
+        });
+
+        Schema::rename('committee_user', 'users_committees_pivot');
+    }
+}
+
+
+
+
+
