dccd3ea18c1b323bcecc9f03d70fbf91606bb437
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2020-01-03 11:59:34 -0800

    put in proper class call in force http statement

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,13 +28,13 @@ class AppServiceProvider extends ServiceProvider
         require app_path('Http/view_helpers.php');
 
         /*
-        view()->composer('page_parts.topics', 'App\Http\ViewComposers@topics');
-        view()->composer('page_parts.content_feature', 'App\Http\ViewComposers@contentFeature');
-        view()->composer('admin.admin_topics_menu', 'App\Http\ViewComposers@adminTopicsMenu');
+        view()->composer('page_parts.topics', 'App\Http\View\Composers\ViewComposers@topics');
+        view()->composer('page_parts.content_feature', 'App\Http\View\Composers\ViewComposers@contentFeature');
+        view()->composer('admin.admin_topics_menu', 'App\Http\View\Composers\ViewComposers@adminTopicsMenu');
          */
 
         if($this->app->environment('production')) {
-            \URL::forceScheme('https');
+            UrlGenerator::forceScheme('https');
         }
 
     }
