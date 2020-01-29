a2dd66cfbc1fc0a512ed2707ec9caa8506527c2e
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2020-01-28 19:16:45 -0800

    migration to drop meeting_attachments table

---
+++ laravel/database/migrations/2020_01_29_030750_drop_meeting_attachments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropMeetingAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::table('meeting_attachments', function (Blueprint $table) {
+            $table->dropForeign('meeting_attachments_meeting_id_foreign');
+
+        });
+
+        Schema::dropIfExists('meeting_attachments');
+        Schema::enableForeignKeyConstraints();
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
