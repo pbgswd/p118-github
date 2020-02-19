782f39338604add28e6af8067c07cbd072a8611b
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2020-02-18 21:44:02 -0800

    public update member form request rules added

--- laravel/app/Http/Requests/Member/UpdateMember.php
+++ laravel/app/Http/Requests/Member/UpdateMember.php
@@ -24,7 +24,22 @@ class UpdateMember extends FormRequest
     public function rules()
     {
         return [
-            //todo updateMember - validation rules for form submission
+            //unique:table,column,except,idColumn
+            'user.name' => 'required|string|max:255',
+            'user.email' => 'required|max:255|unique:users,email,' . $this->route('user')->id . ',id',
+            'user_phone.phone_number' => 'required|max:255',
+            'user_phone.label' => 'string|nullable',
+            'user_phone.primary' => 'boolean',
+            'user_info.share_email' => 'boolean',
+            'user_info.share_phone' => 'boolean',
+            'user_info.image' => 'string|nullable',
+            'user_info.about' => 'string|nullable|max:2000',
+            'user_address.unit' => 'max:255|nullable',
+            'user_address.street' => 'string|required|max:255',
+            'user_address.city' => 'string|required|max:255',
+            'user_address.province' => 'string|required|max:255',
+            'user_address.postal_code' => 'string|required|max:255',
+            'user_address.country' => 'string|required|max:255',
         ];
     }
 }
