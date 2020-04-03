2a30040c85f8216b134cba9a088790df7b3997e7
Vultr server, Peter Gordon <superwebdeveloper@gmail.com>
2020-04-03 07:34:56 +0000

    contact controller submit changed to return redirect

--- laravel/app/Http/Controllers/ContactController.php
+++ laravel/app/Http/Controllers/ContactController.php
@@ -38,15 +38,16 @@ class ContactController extends Controller
     public function submit(SubmitContact $request)
     {
 
+        // $request['email']
         Mail::send('emails.contact', ['data' => $request->all()], function ($m) use ($request) {
-            $m->from($request['email'], $request['name']);
+            $m->from(env('MAIL_USERNAME'), "Local 118 Contact Page Message from " . $request['name']);
             $m->to(env('ADMIN_EMAIL'), env('ADMIN_EMAIL_NAME'))
+                ->replyTo($request['email'], $request['name'])
                 ->subject('Contact Page ' . $request['subject']);
         });
 
         Session::flash('success', 'Your message was sent.');
 
-        return view('contact', ['data' => []]);
+        return redirect()->route('contact');
     }
-
 }
