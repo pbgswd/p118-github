f5a0e80698be7c46aebbc5d30f8c749d0ccd6d7e
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2020-04-21 12:53:44 -0700

    webpack
    changed formatting of webpack for scss, multiple calls, not array

--- laravel/webpack.mix.js
+++ laravel/webpack.mix.js
@@ -8,19 +8,19 @@ const mix = require('laravel-mix');
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
- | .sass('resources/sass/_dropdown.scss', 'public/css').version();
+ | .sass('resources/sass/_dropdown.scss') 'public/css').version();
  */
 
 mix.js('resources/js/popper.min.js', 'public/js')
    .js('resources/js/app.js', 'public/js')
    .js('resources/js/dashboard.js', 'public/js')
    .js('resources/js/tinymce.js', 'public/js')
-    .sass(['resources/sass/app.scss', 'public/css',
-	   'resources/sass/email.scss', 'public/css',	   
-   'resources/sass/dashboard.scss', 'public/css',
-   'resources/sass/jumbotron.scss', 'public/css',
-   'resources/sass/skeleton.scss', 'public/css',
-   'resources/sass/normalize.scss','public/css']).version();
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/email.scss', 'public/css')
+    .sass('resources/sass/dashboard.scss', 'public/css')
+    .sass('resources/sass/jumbotron.scss', 'public/css')
+    .sass('resources/sass/skeleton.scss', 'public/css')
+    .sass('resources/sass/normalize.scss','public/css').version();
 
 //todo tiny mce updates needs to work with all of the mix
 
