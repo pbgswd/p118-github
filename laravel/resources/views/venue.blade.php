88905b22ea177f6423955a0704a8bff28bddb765
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2020-06-19 01:34:49 -0700

    layout work on venue blade
    getting boxes to look ok etc.

--- laravel/resources/views/venue.blade.php
+++ laravel/resources/views/venue.blade.php
@@ -5,49 +5,59 @@ $venue = $data['venue'];
 @section('content')
 <div class="jumbotron">
     <div class="container border border-dark rounded-lg pl-lg-1" style="background: rgba(220,220,220,0.8);">
-        <a href="{{ route('hello') }}">Home /</a>
-        <a href="{{route('venues')}}">venues /</a> {{$venue->name}}
-        <div class="col-12">
-            <h1>{{$venue->name}}</h1>
+        <div class="row">
+            <div class="col-12">
+                <a href="{{ route('hello') }}">Home /</a>
+                <a href="{{route('venues')}}">venues /</a>
+            </div>
+            <div class="col-12">
+                <h1>{{$venue->name}}</h1>
+            </div>
+            <div class="col-12">
+                <p>
+                    <i class="fas fa-external-link-alt"></i>
+                    <a href="{{$venue->url}}" title="{{$venue->name}}" target="_blank">{{$venue->url}}</a>
+                </p>
+            </div>
+            <div class="col-12 p-lg-5">
+                {!! $venue->description !!}
+            </div>
         </div>
-        <div class="col-12">
-            <p>
-                <i class="fas fa-external-link-alt"></i>
-                <a href="{{$venue->url}}" title="{{$venue->name}}" target="_blank">{{$venue->url}}</a>
-            </p>
-        </div>
-        <div class="col-12">
-            {!! $venue->description !!}
-        </div>
-        @if (0 < $data['agreements']->count())
-            <div class="row mt-lg-5 p-lg-5 border border-dark">
+    </div>
+    @if (0 < $data['agreements']->count())
+        <div class="container border border-dark
+                    rounded-lg pl-lg-1 mt-lg-3" style="background: rgba(220,220,220,0.8);">
+            <div class="row mt-lg-5 p-lg-5">
                 <div class="col-12">
-                    Agreements attached to {{$venue->name}}
+                    <h4>Agreements attached to {{$venue->name}}</h4>
                 </div>
-                <div class="col-12">
-                    <table class="table">
-                        <thead></thead>
-                        <tbody>
-                        @foreach($data['agreements'] as $va)
-                            <tr>
-                                <td>
+                <table class="table">
+                    <tbody>
+                    @foreach($data['agreements'] as $va)
+                        <tr>
+                            <td>
+                                <p>
                                     @if(\Carbon\Carbon::parse($va->until)->isPast())
                                         <i>(Not current)</i>
                                     @endif
                                     <a title="{{ $va->title }}" href="{{route('agreement_show', $va->id)}}">
                                         {{ $va->title }}
                                     </a>
-                                </td>
-                                <td>From: {{$va->from->format('F j Y')}}</td>
-                                <td>Until: {{$va->until->format('F j Y')}}</td>
-                            </tr>
-                        @endforeach
-                        <td colspan="3">&nbsp;</td>
-                        </tbody>
-                    </table>
-                </div>
+                                </p>
+                            </td>
+                            <td>
+                                <p>From: {{$va->from->format('F j Y')}}</p>
+                            </td>
+                            <td>
+                                <p>Until: {{$va->until->format('F j Y')}}</p>
+                            </td>
+                        </tr>
+                    @endforeach
+                    <td colspan="4">&nbsp;</td>
+                    </tbody>
+                </table>
             </div>
-        @endif
-    </div>
+        </div>
+    @endif
 </div>
 @endsection
