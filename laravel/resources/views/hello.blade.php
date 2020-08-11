136e884ddd0a405bb9d96710aa68071681f62f7e
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2020-08-10 23:47:42 -0700

    home page layout

--- laravel/resources/views/hello.blade.php
+++ laravel/resources/views/hello.blade.php
@@ -7,19 +7,94 @@
                  style="padding:1em; display: block; margin-left: auto; margin-right: auto;" />
         </div>
     </div>
-    <div class="row border border-light rounded-lg p-lg-2 mb-2" style="background: rgba(220,220,220,0.8);">
-        <div class="col-12 mb-lg-1">
-            <h1>{{config('app.name')}}</h1>
+
+    <div class="row"  style="background: #fff;">
+        <div class="col-12 mb-lg-1 border border-dark rounded-lg" style="height:300px;">
+Carousel
         </div>
     </div>
-    <div class="row border border-light rounded-lg p-lg-2" style="background: rgba(220,220,220,0.8);">
-        <div class="col-12">
+
+    <div class="row mb-2 mt-lg-5">
+        <div class="w-50 col-6 border border-dark rounded-lg pt-2">
+            <h1><i class="far fa-newspaper"></i> News & Highlights</h1>
+            <ul>
+                <li>News </li>
+                <li>News </li>
+                <li>News </li>
+                <li>News </li>
+                <li>News </li>
+                <li>News </li>
+                <li>News </li>
+            </ul>
+        </div>
+        <div class="w-50 col-6">
+            <div class="col-12 border border-dark rounded-lg mb-lg-1 pt-2">
+                <h1><i class="far fa-calendar-alt"></i> Upcoming Shows</h1>
+                <ul>
+                    <li>Show</li>
+                    <li>Show</li>
+                    <li>Show</li>
+                    <li>Show</li>
+                    <li>Show</li>
+                </ul>
+            </div>
+
+            <div class="col-12 border border-dark rounded-lg">
+                <h2>
+                    <i class="fas fa-hashtag"></i> Social Media
+                </h2>
+                <div class="row">
+                    <div class="col-6 border border-dark rounded-lg">
+                        <ul>
+                            <li>
+                                <a href="https://twitter.com/IATSE_118" target="_blank" title="IATSE Local 118">
+                                    <i class="fab fa-twitter"></i>
+                                    @IATSE_118
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/IATSECANADA" target="_blank" title="IATSE Canada">
+                                    <i class="fab fa-twitter"></i>
+                                    @IATSECANADA
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/IATSEYWC" target="_blank" title="IATSE Young Workers">
+                                    <i class="fab fa-twitter"></i>
+                                    @IATSEYWC
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/IATSE" target="_blank" title="IATSE">
+                                    <i class="fab fa-twitter"></i>
+                                    @IATSE
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-6 border border-dark rounded-lg">
+                        2
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 border border-dark rounded-lg">
+                        3
+                    </div>
+                    <div class="col-6 border border-dark rounded-lg">
+                        4
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mt-lg-5" style="background: rgba(220,220,220,0.8);">
+        <div class="col-12 border border-light rounded-lg p-lg-4">
             @if($data['birthday'] != '')
                 <h2> <i class="fas fa-birthday-cake"></i> {{ $data['birthday'] }}</h2>
             @endif
             <h3>{{$data['years']}} years, since {{$data['foundingYear']}}.</h3>
-        </div>
-        <div class="col-12">
+
             <p>
                 <span class="dropcap">F</span>ounded on September 13, 1904,
                 IATSE Local 118 (International Alliance of Theatrical Stage Employees of the United States and Canada)
