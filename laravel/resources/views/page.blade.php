1b909dcc7568e5faa0574d9615d91bcbf3d7834a
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2020-08-13 00:44:16 -0700

    changes to single page, post layout

--- laravel/resources/views/page.blade.php
+++ laravel/resources/views/page.blade.php
@@ -6,51 +6,53 @@ $tags = join(', ', $page->tagNames());
 @section('content')
 <div class="jumbotron">
     <div class="container border border-dark rounded-lg" style="background: rgba(220,220,220,0.8);">
-        <div  class="col-12">
-            <h1 class="display-3">{{$page->title}}</h1>
-        </div>
-        <div class="col-12">
-            <h2>{!! $page->description !!}</h2>
-        </div>
-        <div class="col-12">
-            {!! $page->content !!}
-         </div>
-        @if(count($page->attachments) > 0)
-            <div class="col-md-12">
-                <h4>Files</h4>
-                <table class="table table-striped table-sm">
-                    <thead>
-                    <tr>
-                        <th> File </th>
-                        <th> Description </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($page->attachments as $pa)
+        <div class="row mb-lg-5">
+            <div  class="col-4">
+                Left Col 
+            </div>
+            <div  class="col-8">
+                <div  class="col-12">
+                    <h1 class="display-3">{{$page->title}}</h1>
+                </div>
+                <div class="col-12">
+                    <h2>{!! $page->description !!}</h2>
+                </div>
+                <div class="col-12">
+                    {!! $page->content !!}
+                 </div>
+                @if(count($page->attachments) > 0)
+                    <div class="col-md-12">
+                        <h4>Files</h4>
+                        <table class="table table-striped table-sm">
+                            <thead>
                             <tr>
-                                <td>
-                                    <a href="{{route('attachment_download', [$page->getAttachmentFolder(), $pa->id])}}" title="Download {{$pa->file_name}}">{{$pa->file_name}}</a>
-                                </td>
-                                <td>
-                                    {{ $pa->description}}
-                                </td>
+                                <th> File </th>
+                                <th> Description </th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($page->attachments as $pa)
+                                    <tr>
+                                        <td>
+                                            <a href="{{route('attachment_download', [$page->getAttachmentFolder(), $pa->id])}}" title="Download {{$pa->file_name}}">{{$pa->file_name}}</a>
+                                        </td>
+                                        <td>
+                                            {{ $pa->description}}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                @endif
+                @if ($tags != '')
+                    <div class="col-md-12">
+                        Tags: {{$tags}}
+                    </div>
+                @endif
             </div>
-        @endif
-        @if ($tags != '')
-            <div class="row">
-                Tags: {{$tags}}
-            </div>
-        @endif
-        <div class="row">
-            <a href="{{route('pages')}}">Pages / </a> &nbsp;
-            {{$page->title}}
         </div>
     </div>
-<div class="row mt-lg-5"></div>
 </div>
 @endsection
 
--- laravel/resources/views/post.blade.php
+++ laravel/resources/views/post.blade.php
@@ -8,58 +8,57 @@ $tags = join(', ', $post->tagNames());
     <div class="container border border-dark rounded-lg pt-lg-2" style="background: rgba(220,220,220,0.8);">
         <div class="row">
             @foreach ($post->topics as $topic)
-                <a href="{{ route('hello') }}">Home /</a>
-                &nbsp;<a href="{{route('topics')}}">Topics /</a>
-                &nbsp;<a href="{{ route('topic_show', $topic->slug) }}">{{$topic->name}} /</a>
-                &nbsp; {{$post->title}}
+                <div class="col-12">
+                <a href="{{route('topics')}}">Topics/</a>
+                    <a href="{{ route('topic_show', $topic->slug)}}">{{$topic->name}}/</a>
+            </div>
             @endforeach
         </div>
-        <div  class="col-12">
-            <h1 class="display-5">{{$post->title}}</h1>
-        </div>
-        <div class="col-12">
-            <h3>{!! $post->description !!}</h3>
-        </div>
-        <div class="col-12">
-            {!! $post->content !!}
-         </div>
-        @if(count($post->attachments) > 0)
-            <div class="col-md-12">
-                <h4>Files</h4>
-                <table class="table table-striped table-sm">
-                    <thead>
-                    <tr>
-                        <th> File </th>
-                        <th> Description </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($post->attachments as $pa)
-                        <tr>
-                            <td>
-                                <a href="{{route('attachment_download', [$post->getAttachmentFolder(), $pa->id])}}" title="Download {{$pa->file_name}}">{{$pa->file_name}}</a>
-                            </td>
-                            <td>
-                                {{ $pa->description}}
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+        <div class="row mb-lg-5">
+            <div class="col-4 border border-dark rounded-lg">xxx</div>
+            <div class="col-8 border border-dark rounded-lg">
+                <div class="col-12">
+                    <h1 class="display-5">{{$post->title}}</h1>
+                </div>
+                <div class="col-12">
+                    <h3>{!! $post->description !!}</h3>
+                </div>
+                <div class="col-12">
+                    {!! $post->content !!}
+                </div>
+                @if(count($post->attachments) > 0)
+                    <div class="col-md-12">
+                        <h4>Files</h4>
+                        <table class="table table-striped table-sm">
+                            <thead>
+                            <tr>
+                                <th> File </th>
+                                <th> Description </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($post->attachments as $pa)
+                                <tr>
+                                    <td>
+                                        <a href="{{route('attachment_download', [$post->getAttachmentFolder(), $pa->id])}}" title="Download {{$pa->file_name}}">{{$pa->file_name}}</a>
+                                    </td>
+                                    <td>
+                                        {{ $pa->description}}
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                @endif
+                @if ($tags != '')
+                    <div class="row">
+                        Tags: {{$tags}}
+                    </div>
+                @endif
             </div>
-        @endif
-
-        @if ($tags != '')
-            <div class="row">
-                Tags: {{$tags}}
-            </div>
-        @endif
-        <div class="row">
-            <a href="{{route('posts')}}">Posts /</a> &nbsp;
-                {{$post->title}}
         </div>
     </div>
-    <div class="row mt-lg-5"></div>
 </div>
 @endsection
 
