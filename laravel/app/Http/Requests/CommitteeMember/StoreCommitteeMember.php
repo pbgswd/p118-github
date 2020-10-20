348a71210b1ca438e8a058962155e20b6bef6068
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2020-10-20 16:34:45 -0700

    form request files for store and search committee membership

---
+++ laravel/app/Http/Requests/CommitteeMember/StoreCommitteeMember.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\CommitteeMember;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCommitteeMember extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required|exists:user',
+            'role' => 'required|string',
+            'committee_id' => 'required|int|exists:committees'
+        ];
+    }
+}
