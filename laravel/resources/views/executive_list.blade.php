0ef3c1086bf9f6f6c91ba430fd45e7ae9b903238
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-01-04 18:37:36 -0800

    convert to table

--- laravel/resources/views/executive_list.blade.php
+++ laravel/resources/views/executive_list.blade.php
@@ -10,31 +10,59 @@
         <div class="col-12">
             <h1> Executive </h1>
         </div>
-         @foreach($data as $e)
-            <div class="col-12 mb-3">
-                <h3>{{$e->title}}</h3>
-                 @if( !empty($e->user[0]))
-                    @foreach($e->current_executive_user as $exec)
-                        <h5>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col" colspan="3"></th>
+                    <th scope="col">From</th>
+                    <th scope="col">Until</th>
+                </tr>
+            </thead>
+            <tbody>
+            @forelse($data as $e)
+                <tr>
+                    <td colspan="5">
+                        <h3>{{$e->title}}</h3>
+                    </td>
+                </tr>
+                @forelse($e->current_executive_user as $exec)
+                <tr>
+                    <td> &nbsp;</td>
+                    <td>
+                        <h4>
                             @auth
                                 <a title="{{ $exec->name }}" href="{{ route('member', $exec->id) }}">
-                            @endauth
-                            {{$exec->name ?? ''}}
-                            @auth
+                                    @endauth
+                                    {{$exec->name ?? ''}}
+                                    @auth
                                 </a>
                             @endauth
-                            <a href="mailto:{{$e->email}}">
-                                <i class="fas fa-envelope"></i>
-                            </a>
-                            {{\Carbon\Carbon::parse($exec->pivot->start_date)->format('M j Y')}}
-                            - {{\Carbon\Carbon::parse($exec->pivot->end_date)->format('M j Y')}}
-                        </h5>
-                    @endforeach
-                @else
-                    <i>(To be assigned)</i>
-                @endif
-            </div>
-        @endforeach
+                        </h4>
+                    <td>
+                        <a href="mailto:{{$e->email}}">
+                            <i class="fas fa-envelope"></i>
+                        </a>
+                    </td>
+                    <td>
+                        {{\Carbon\Carbon::parse($exec->pivot->start_date)->format('M j Y')}}
+                    </td>
+                    <td>
+                        {{\Carbon\Carbon::parse($exec->pivot->end_date)->format('M j Y')}}
+                    </td>
+                </tr>
+                @empty
+                    <tr>
+                        <td colspan="5">No entry</td>
+                    </tr>
+                @endforelse
+            @empty
+                <tr>
+                    <td colspan="5">No entry</td>
+                </tr>
+            @endforelse
+            </tbody>
+        </table>
+
     </div>
 </div>
 @endsection
