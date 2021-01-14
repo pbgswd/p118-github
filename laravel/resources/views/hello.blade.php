986b2cfc0e14270cd710c06f2ef5a14daf9449d3
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-01-13 18:42:29 -0800

    bottom margin on years box

--- laravel/resources/views/hello.blade.php
+++ laravel/resources/views/hello.blade.php
@@ -8,7 +8,7 @@
         </div>
     </div>
 
-    <div class="row mt-lg-4">
+    <div class="row mt-4 mb-3">
         <div class="col-12 border border-dark rounded-lg p-lg-4">
             @if($data['birthday'] != '')
                 <h2>
