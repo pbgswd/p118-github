16cfdfe1a8c267fce0a5c83f17981ea732b4a682
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-01-22 21:56:18 -0800

    layout improvements to new section, history statement.

--- laravel/resources/views/layouts/history-statement.blade.php
+++ laravel/resources/views/layouts/history-statement.blade.php
@@ -1,15 +1,15 @@
-<div class="row mt-3 mb-lg-5">
+<div class="row mt-3 mb-lg-5 p-lg-2">
     <div class="col-12 border border-dark rounded-lg p-lg-4">
         @if($data['birthday'] != '')
-            <h2>
+            <h2 class="pt-2">
                 <i class="fas fa-birthday-cake"></i> {{ $data['birthday'] }}
             </h2>
         @endif
-        <h3>
+        <h3 class="pt-2">
             {{$data['years']}} years, since {{$data['foundingYear']}}.
         </h3>
         <p>
-            <span class="dropcap">F</span>ounded on September 13, 1904,
+            <span class="font-weight-bolder">F</span>ounded on September 13, 1904,
             IATSE Local 118 (International Alliance of Theatrical Stage Employees of the United States
             and Canada) the labour union supplying technicians, stagehands, artisans and craftspeople to
             the Greater Vancouver entertainment industry, including live theatre, rock and roll, trade
