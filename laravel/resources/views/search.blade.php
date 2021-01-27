eb0692a646fef6641026ab9a04736f08cc246551
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-01-26 18:55:47 -0800

    search layout update

--- laravel/resources/views/search.blade.php
+++ laravel/resources/views/search.blade.php
@@ -1,51 +1,51 @@
-<?php
-
-?>
 @extends('layouts.jumbo')
 @section('content')
 <div class="jumbotron">
-    <div class="container border border-dark rounded-lg pt-lg-2" style="background: rgba(220,220,220,0.8);">
+    <div class="container border border-dark rounded-lg pt-2" style="background: rgba(220,220,220,0.8);">
         <div class="col-12">
-            <h1 class="display-8">Search Results</h1>
+            <h1>
+                {{ $data['results']->count() }}
+                Search  {{ Str::plural('Result', $data['results']->count()) }}
+                for "{{$data['search']}}"
+            </h1>
         </div>
-        <div class="col-12">
-            <h3>
-                {{ $data['results']->count() }} {{ Str::plural('Result', $data['results']->count()) }} for "{{$data['search']}}"
-            </h3>
-        </div>
-        <!-- Example row of columns -->
         <div class="row">
             @if($data['results']->count() < 1)
-                <div class="col-10 border border-dark rounded-lg mt-3 mr-3">
-                    <h2>Modify your search term and try again in the search field, or use the navigation links to find what you are looking for.</h2>
-                    <p><a class="btn btn-primary btn-lg" href="/" role="button">Return To Home Page</a></p>
+                <div class="col-12 border border-dark rounded-lg p-2">
+                    <h4>
+                        Sorry that didn't get you what you were looking for.
+                        You can always modify your search term and try another search.
+                    </h4>
                 </div>
-            @else
-                @foreach ( $data['results'] as $r )
-                    <div class="col-3 border border-dark rounded-lg mt-3 mr-3">
-                        <h2>{{$r->title}}</h2>
-                        <p>
-                            <a class="btn btn-secondary" href="{{$r->url}}" role="button">View details &raquo;</a>
-                        </p>
-                    </div>
-                @endforeach
+            @endif
+            @if($data['results']->count() > 30)
+                <div class="col-12 border border-dark rounded-lg p-2">
+                    <h4>
+                        {{ $data['results']->count() }} results from your search is a lot!
+                        Consider adding more terms to your search to find what you are looking for.
+                    </h4>
+                </div>
             @endif
         </div>
-    <div class="row">
-        <div class="col"></div>
-        <div class="col">
-            <div class="list-group">
-                <ul class="pagination">
-
-                </ul>
-            </div>
+        <div class="row">
+            @forelse ( $data['results'] as $r )
+                <div class="col-md-4 col-sm-12 justify-content-around p-2">
+                    <div class="col border border-dark rounded-lg p-2">
+                        <a href="{{$r->url}}" title="{{$r->title}}">
+                             <h3>{{$r->title}}</h3>
+                        </a>
+                    </div>
+                </div>
+            @empty
+                <div class="col-12 justify-content-around p-2">
+                    <div class="col border border-dark rounded-lg p-2">
+                         No results!
+                    </div>
+                </div>
+            @endforelse
         </div>
-        <div class="col"></div>
     </div>
 </div>
-
-<div class="row mt-lg-5"></div>
-
 @endsection
 
 
