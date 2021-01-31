8e4992483114839c5ee4c87af7edf08f1224c72d
Peter Gordon Windows Laptop <superwebdeveloper@gmail.com>
2021-01-31 00:24:34 -0800

    wording change

--- laravel/resources/views/site.blade.php
+++ laravel/resources/views/site.blade.php
@@ -20,7 +20,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="alert alert-primary" role="alert">
-                        Please add your personal information and update the visibility settings to your
+                        Please review your personal information and visibility settings in your
                         <span class="font-weight-bolder">
                             <a href="{{route('member', $data['user']->id)}}" title="My Profile">
                                 personal profile.
