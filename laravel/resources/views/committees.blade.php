bf549e3784470eb23b9e76f517cff490e2855b8a
Peter Gordon Windows Laptop <superwebdeveloper@gmail.com>
2021-02-07 21:41:41 -0800

    removed member count from layout

--- laravel/resources/views/committees.blade.php
+++ laravel/resources/views/committees.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container border border-dark rounded-lg mb-5 mt-3" style="background: rgba(220,220,220,0.8);">
     <div class="row">
-        <div class="col-12 mb-3 mt-3 text-center">
+        <div class="col-12 mb-3 text-center">
             <h1>
                 Local 118 Committees
             </h1>
@@ -18,10 +18,6 @@
                         </a>
                     </h3>
                     <p>{!! $c->description !!}</p>
-                    <p>
-                        {{$c->active_committee_members->count()}}
-                        {{Str::plural('member', $c->active_committee_members->count())}}.
-                    </p>
                 </div>
             </div>
         @empty
