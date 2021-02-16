e4f6bc1fa916cfd9f736babf5866eafc20c6e24d
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-02-16 14:56:32 -0800

    found the topics blade and restored it

--- laravel/resources/views/topics.blade.php
+++ laravel/resources/views/topics.blade.php
@@ -1,48 +1,27 @@
 @extends('layouts.jumbo')
 @section('content')
-<div class="jumbotron">
-    <div class="container border border-dark rounded" style="background: rgba(220,220,220,0.8);">
-        <div class="row">
-            <div class="col-0 col-md-4"></div>
-            <div class="col-12 col-md-4 text-center">
-                <h1 class="text-center">Topics</h1>
-            </div>
-            <div class="col-0 col-md-4 text-right">
-                @can('edit articles')
-                    <a href="{{route('topics_list')}}">Admin</a>
-                @endcan
-            </div>
+    <div class="container border border-dark rounded-lg mb-1 mb-lg-3" style="background: rgba(220,220,220,0.8); padding:2em;">
+        <div class="col-12 text-center">
+            <h1>Topics</h1>
         </div>
-        <div class="row m-0">
+        <div class="row mb-2 mb-lg-3">
             @foreach ( $data['topics'] as $i )
-                <div class="col-12 col-md-4 p-2">
-                    <div class="col p-2 m-2 border border-dark rounded w-100 h-100">
-                        <h5>
-                            <a href="{{ route('topic_show', $i->slug) }}" title="{{$i->name}}">
-                                {{ $i->name }}
-                            </a>
-                        </h5>
-                        <p>
-                            {!! $i->description !!}
-                        </p>
+                <div class="col-12 col-md-4 p-1">
+                    <div class="col border border-dark rounded-lg  w-100 h-100 p-2">
+                        <a href="{{ route('topic_show', $i->slug) }}" title="{{ $i->name }}">
+                            <h3>{{ $i->name }}</h3>
+                            <p>{!! $i->description !!} </p>
+                        </a>
                     </div>
                 </div>
             @endforeach
         </div>
-    <div class="row">
-        <div class="col-0 col-md-4"></div>
-        <div class="col-12 col-md-4 text-center pt-5">
+        <div class="d-flex justify-content-center">
             <div class="list-group">
                 <ul class="pagination">
                     {!! $data['topics']->links() !!}
                 </ul>
             </div>
         </div>
-        <div class="col-0 col-md-4"></div>
     </div>
-</div>
 @endsection
-
-
-
-
