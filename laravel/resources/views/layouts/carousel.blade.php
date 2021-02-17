878aabda7ed674d6e864b3cf5f9b4d2097a1d4f5
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-02-17 00:56:14 -0800

    carousel blade for front page

---
+++ laravel/resources/views/layouts/carousel.blade.php
@@ -0,0 +1,127 @@
+    <link href="{{ mix('css/carousel.css') }}" rel="stylesheet">
+    <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="6000">
+        <ol class="carousel-indicators">
+            <li data-target="#carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel" data-slide-to="1"></li>
+            <li data-target="#carousel" data-slide-to="2"></li>
+            <li data-target="#carousel" data-slide-to="3"></li>
+            <li data-target="#carousel" data-slide-to="4"></li>
+            <li data-target="#carousel" data-slide-to="5"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+                <a href="/venue/queen-elizabeth-theatre">
+                    <picture>
+                        <source srcset="/storage/public/i60UUz1g1yDBvPXeGF7IKlnm9Gyg5wDe742kVvu2.jpg" media="(min-width: 1400px)">
+                        <source srcset="/storage/public/enqgGnQCepp4ks4aX2uFos5qgJsWFc2xaVUeSRj6.jpg" media="(min-width: 769px)">
+                        <source srcset="/storage/public/NKMSf6rHEcLDGX9Z44K8vbuPpnsCtjMvCv2IHUkt.jpg" media="(min-width: 577px)">
+                        <img srcset="/storage/public/DhMkV9JVZBgvLhqYSwUmcDRInIKvxL7G0UBpHgwA.jpg" alt="Queen Elizabeth Theatre" class="d-block img-fluid">
+                    </picture>
+
+                    <div class="carousel-caption">
+                        <div>
+                            <h2>The Performing Arts</h2>
+                            <p>We bring your production to the stage</p>
+                            <span class="btn btn-sm btn-outline-secondary">Learn More</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <!-- /.carousel-item -->
+
+            <div class="carousel-item">
+                <a class="" href="/hire-us">
+
+                    <picture>
+                        <source srcset="/storage/public/7W7f1c0gLCLNnx6SNZ4VWRwYal2W8OyV9UUsFW2r.jpg" media="(min-width: 1400px)">
+                        <source srcset="/storage/public/4mugQjHTNHDRJ8A6BUngTb8nWGbJJYk7ibuJ99kv.jpg" media="(min-width: 769px)">
+                        <source srcset="/storage/public/I7a89I0vwAmRpZ19FKcRq6SCtXPOmD0wrInL6o9a.jpg" media="(min-width: 577px)">
+                        <img srcset="/storage/public/2ZCdQ3uRUhFe5W9GSUnEEB0tXZQufSwMiOziOYYD.jpg" alt="Fly Stage" class="d-block img-fluid">
+                    </picture>
+
+                    <div class="carousel-caption justify-content-center align-items-center">
+                        <div>
+                            <h2>We know the ropes</h2>
+                            <p>We have the skills to bring your production to new heights</p>
+                            <span class="btn btn-sm btn-outline-secondary">Our Services</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <!-- /.carousel-item -->
+            <div class="carousel-item">
+                <a class="" href="http://p118">
+
+                    <picture>
+                        <source srcset="/storage/public/OgN7XgSakf9F28tLVSAwXbj3dyWjmjwvIGL7vhmE.jpg" media="(min-width: 1400px)">
+                        <source srcset="/storage/public/a1DnMfzCZQ914j0pW5N0tojuzeU8oABaeybNHzCB.jpg" media="(min-width: 769px)">
+                        <source srcset="/storage/public/VvSG5f3Vtdo1GQixukCL8JU6gYL3bsBTHAwjyRBB.jpg" media="(min-width: 577px)">
+                        <img srcset="/storage/public/L14QT2AUPtArxCUdM1Zb5sLzZQSD0xu8MVFvk8rQ.jpg" alt="Orpheum Theatre" class="d-block img-fluid">
+                    </picture>
+
+                    <div class="carousel-caption justify-content-center align-items-center">
+                        <div>
+                            <h2>Stars Day and Night</h2>
+                            <p>We build your spectacle</p>
+                            <span class="btn btn-sm btn-outline-secondary">Our Process</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <!-- /.carousel-item -->
+
+            <div class="carousel-item">
+                <a class="" href="http://p118">
+
+                    <picture>
+                        <source srcset="/storage/public/vzK5ybAkREaQWIT1Aorz18tYfEJIX1Fzec0ktGkd.jpg" media="(min-width: 1400px)">
+                        <source srcset="/storage/public/xcfte15i5kqgZaGDvKEH48teIu5w6y0yYCkgcupd.jpg" media="(min-width: 769px)">
+                        <source srcset="/storage/public/WFPqI5eTzlW9TYViU865c5mRGaKHtNBGzNsBEFwu.jpg" media="(min-width: 577px)">
+                        <img srcset="/storage/public/GOIUPES470m9xrX3q1K1DvWzuXpjhEyg5I2Lxl8A.jpg" alt="PNE" class="d-block img-fluid">
+                    </picture>
+
+                    <div class="carousel-caption text-left justify-content-left align-items-left">
+                        <div>
+                            <h2>Celebrate the world</h2>
+                            <p>Performers from the entire world rely on us</p>
+                            <span class="btn btn-sm btn-outline-secondary">Our Process</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <!-- /.carousel-item -->
+            <div class="carousel-item">
+                <a class="" href="http://p118">
+
+                    <picture>
+                        <source srcset="/storage/public/F367z8aDhM6nYcTRaI1FS47IRGXha6F2oYE37VJ0.jpg" media="(min-width: 1400px)">
+                        <source srcset="/storage/public/NKyEgYBOkxkJyGg9gV8RmHmEG8eyNUiMxihblNUW.jpg" media="(min-width: 769px)">
+                        <source srcset="/storage/public/ofdOoXAOzfjYiJrYeRDLwAJRKD0SAMuiDH3tRA7k.jpg" media="(min-width: 577px)">
+                        <img srcset="/storage/public/p5uTtyJcYSlvGjz04wIy5GBGVwNxDvYCoW1r3tPH.jpg" alt="Abbotsford Centre" class="d-block img-fluid">
+                    </picture>
+
+                    <div class="carousel-caption justify-content-center align-items-center text-dark">
+                        <div>
+                            <h2>Super Shows</h2>
+                            <p>We deliver your performance to the stage</p>
+                            <span class="btn btn-sm btn-outline-secondary">Our Process</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <!-- /.carousel-item -->
+        </div>
+        <!-- /.carousel-inner -->
+        <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <!-- /.carousel -->
+
