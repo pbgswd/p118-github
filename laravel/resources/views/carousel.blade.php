1c0769ceddd8b8bd08c737e86d186a345daf53ed
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-02-17 00:54:29 -0800

    first draft, slide show on home page

--- laravel/resources/views/carousel.blade.php
+++ laravel/resources/views/carousel.blade.php
@@ -1,70 +1,90 @@
 @extends('layouts.jumbo')
 @section('content')
-    <link href="{{ mix('css/carousel.css') }}" rel="stylesheet">
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-
+    <link href="{{ mix('css/carousel2.css') }}" rel="stylesheet">
+    <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="6000">
         <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel" data-slide-to="1"></li>
+            <li data-target="#carousel" data-slide-to="2"></li>
         </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active mt-5">
-                <picture>
-                    <source srcset="https://dummyimage.com/2000x500/007aeb/4196e5" media="(min-width: 1400px)">
-                    <source srcset="https://dummyimage.com/1400x500/#007aeb/4196e5" media="(min-width: 769px)">
-                    <source srcset="https://dummyimage.com/800x500/007aeb/4196e5" media="(min-width: 577px)">
-                    <img srcset="https://dummyimage.com/600x500/007aeb/4196e5" alt="responsive image" class="d-block img-fluid">
-                </picture>
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+                <a href="https://bootstrapcreative.com/">
+                    <picture>
+                        <source srcset="https://dummyimage.com/2000x500/007aeb/4196e5" media="(min-width: 1400px)">
+                        <source srcset="https://dummyimage.com/1400x500/#007aeb/4196e5" media="(min-width: 769px)">
+                        <source srcset="https://dummyimage.com/800x500/007aeb/4196e5" media="(min-width: 577px)">
+                        <img srcset="https://dummyimage.com/600x500/007aeb/4196e5" alt="responsive image" class="d-block img-fluid">
+                    </picture>
 
-                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" role="img" aria-label=" :  " preserveAspectRatio="xMidYMid slice" focusable="false"><title> </title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em"> </text></svg>
-
-                <div class="container">
-
-                    <div class="carousel-caption text-left">
-                        <h1>Demo Photos</h1>
-                        <p>Some representative placeholder content for the first slide of the carousel.</p>
-                            <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <picture>
-                    <source srcset="/storage/public/ePnuaj0PPPTBGKXoXoXk3NoIpzjRcMlMkAzndrRd.jpg" media="(min-width: 1400px)">
-                    <source srcset="/storage/public/ePnuaj0PPPTBGKXoXoXk3NoIpzjRcMlMkAzndrRd.jpg" media="(min-width: 577px)">
-                    <img src="/storage/public/ePnuaj0PPPTBGKXoXoXk3NoIpzjRcMlMkAzndrRd.jpg" class="d-block img-fluid">
-                </picture>
-                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" role="img" aria-label=" :  " preserveAspectRatio="xMidYMid slice" focusable="false"><title> </title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em"> </text></svg>
-
-
-                <div class="container">
                     <div class="carousel-caption">
-                        <h1>Another example headline.</h1>
-                        <p>Some representative placeholder content for the second slide of the carousel.</p>
-                        <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+                        <div>
+                            <h2>Digital Craftsmanship</h2>
+                            <p>We meticously build each site to get results</p>
+                            <span class="btn btn-sm btn-outline-secondary">Learn More</span>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
-            <div class="carousel-item">
-                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" role="img" aria-label=" :  " preserveAspectRatio="xMidYMid slice" focusable="false"><title> </title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em"> </text></svg>
+            <!-- /.carousel-item -->
 
-                <div class="container">
-                    <div class="carousel-caption text-right">
-                        <h1>One more for good measure.</h1>
-                        <p>Some representative placeholder content for the third slide of this carousel.</p>
-                        <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+            <div class="carousel-item">
+                <a class="" href="http://p118">
+
+                    <picture>
+                        <source srcset="/storage/public/OtHJNvvtswevHdoGcvUWRspQeUBpcKA7w6rEXp8w.jpg" media="(min-width: 1400px)">
+                        <source srcset="/storage/public/ePnuaj0PPPTBGKXoXoXk3NoIpzjRcMlMkAzndrRd.jpg" media="(min-width: 769px)">
+                        <source srcset="https://dummyimage.com/800x500/007aeb/4196e5" media="(min-width: 577px)">
+                        <img srcset="https://dummyimage.com/600x500/007aeb/4196e5" alt="responsive image" class="d-block img-fluid">
+                    </picture>
+
+                    <div class="carousel-caption justify-content-center align-items-center">
+                        <div>
+                            <h2>Every project begins with a sketch</h2>
+                            <p>We work as an extension of your business to explore solutions</p>
+                            <span class="btn btn-sm btn-outline-secondary">Our Process</span>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
+            <!-- /.carousel-item -->
+            <div class="carousel-item">
+                <a href="https://bootstrapcreative.com/">
+                    <picture>
+                        <source srcset="https://dummyimage.com/2000x500/007aeb/4196e5" media="(min-width: 1400px)">
+                        <source srcset="https://dummyimage.com/1400x500/007aeb/4196e5" media="(min-width: 769px)">
+                        <source srcset="https://dummyimage.com/800x500/007aeb/4196e5" media="(min-width: 577px)">
+                        <img srcset="https://dummyimage.com/600x500/007aeb/4196e5" alt="responsive image" class="d-block img-fluid">
+                    </picture>
+
+                    <div class="carousel-caption justify-content-center align-items-center">
+                        <div>
+                            <h2>Performance Optimization</h2>
+                            <p>We monitor and optimize your site's long-term performance</p>
+                            <span class="btn btn-sm btn-secondary">Learn How</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <!-- /.carousel-item -->
         </div>
-        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+        <!-- /.carousel-inner -->
+        <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
     </div>
+    <!-- /.carousel -->
+
+
+    <div class="container-fluid text-center">
+        <p>Full width carousel with a maximum height and art direction. Resize window to see image change based on the size of the window.</p>
+    </div>
+    <!-- /.container -->
+
 
 @endsection
--- laravel/resources/views/hello.blade.php
+++ laravel/resources/views/hello.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.jumbo')
+@include('layouts.carousel')
 @section('content')
-<div class="container">
+    <div class="container">
     <div class="row">
-        <div class="col-12 mb-lg-1">
+        <div class="col-12 mb-1">
             <img src="/storage/public/64tsEl26mhTFapH4Rco0QidSjj5yMx9s0cJfePq8.png"
                  style="padding:1em; display: block; margin-left: auto; margin-right: auto;" alt="{{env('APP_NAME')}}"  class="img-fluid rounded"/>
         </div>
     </div>
+
     @include('layouts.history-statement')
     @include('layouts.news-highlights')
 </div>
