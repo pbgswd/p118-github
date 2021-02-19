38a10a91ca2a48666971a36d5685cdfb6eee427d
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-02-19 01:10:31 -0800

    org, venue layout centering

--- laravel/resources/views/organizations.blade.php
+++ laravel/resources/views/organizations.blade.php
@@ -9,11 +9,11 @@
         <div class="row p-3">
             @foreach ( $data['organizations'] as $organization )
                 <div class="col-12 col-md-4 mt-3 p-2">
-                    <div class="border border-dark rounded-lg h-100 w-100 p-2 text-center">
-                        <a href="{{ route('organization', $organization->slug) }}">
+                    <div class="border border-dark rounded-lg h-100 w-100 p-2 text-center d-flex align-items-center justify-content-center">
+                        <a href="{{ route('organization', $organization->slug) }}" class="">
                             @if($organization->image)
                                 <img src="{{asset('storage/public/'. $data['tn_prefix'].$organization->image)}}"
-                                     class="img-fluid rounded" title="{{$organization->name}}"/>
+                                     class="img-fluid rounded" title="{{$organization->name}}"/> <br />
                             @endif
                             <h2>{{ $organization->name }}</h2>
                         </a>
