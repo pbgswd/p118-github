e4420b2e5e4029b134cf7facd64cacfcea9aab96
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-02-22 13:54:23 -0800

    centering content of contact page

--- laravel/resources/views/contact.blade.php
+++ laravel/resources/views/contact.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.jumbo')
 @section('content')
     <div class="container mb-3" style="background: rgba(220,220,220,0.8);">
-        <div class="row mt-3 mb-2 border border-dark rounded-lg">
+        <div class="row mt-3 m-2 border border-dark rounded-lg">
             <div class="col-12 p-2 text-center">
                 <h2>Contact IATSE Local 118</h2>
             </div>
@@ -24,10 +24,12 @@
                     <input type="text" class="form-control" placeholder="Subject" name="mail_subject"
                            value="{{ old('mail_subject')}}" size="80" required/>
                 </div>
-                <div class="col-12 input-group mt-2 mb-2">
+                <div class="col-12 input-group mt-2 d-flex justify-content-center">
                     <label for="mail_body" class="col-sm-2 control-label">
                         <h3>Message</h3>
                     </label>
+                </div>
+                <div class="col-12 input-group mt-2 mb-2 d-flex justify-content-center">
                     <textarea name="mail_body" placeholder="Message" class="form-control input-lg" rows="3"
                     cols="100">{{old('mail_body')}}</textarea>
                 </div>
