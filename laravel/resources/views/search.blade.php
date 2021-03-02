bbed457e21ac202500d8064ccd96ae08f9912a58
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-03-01 20:16:26 -0800

    model list with routes for search, search blade

--- laravel/resources/views/search.blade.php
+++ laravel/resources/views/search.blade.php
@@ -11,22 +11,23 @@
                 </h3>
             </div>
         </div>
-        <div class="row">
-            @if($data['results']->count() > 30)
+        @if($data['results']->count() > 30)
+            <div class="row">
                 <div class="col-12 border border-dark rounded-lg p-2">
                     <h4>
                         {{ $data['results']->count() }} results from your search is a lot!
                         Consider adding more terms to your search to find what you are looking for.
                     </h4>
                 </div>
-            @endif
-        </div>
+            </div>
+        @endif
         <div class="row">
             @forelse ( $data['results'] as $r )
                 <div class="col-12 col-md-4 p-2">
                     <div class="col border border-dark rounded-lg w-100 h-100 p-2">
                         @if($r->searchable->info)
-                            <a href="{{route($r->searchable->info['route'])}}" title="{{$r->searchable->info['name']}}">
+                            <a href="{{route($r->searchable->info['pub_route_list'])}}"
+                               title="{{$r->searchable->info['name']}}">
                                 {{$r->searchable->info['name']}}
                             </a>
                         @endif
