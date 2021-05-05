d28527d78fe77834ef3a395bc25a71bc87a3fac5
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-05-04 19:34:43 -0700

    killed some blank lines

--- laravel/resources/views/layouts/jumbo.blade.php
+++ laravel/resources/views/layouts/jumbo.blade.php
@@ -37,8 +37,6 @@
         <link rel="icon" href="/icon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png"><!-- 180×180 -->
         <link rel="manifest" href="/manifest.webmanifest">
-
-
     </head>
     <body>
         <a id="top"></a>
