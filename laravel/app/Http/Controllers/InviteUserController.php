c878533e9aabe51b3bc95af7e720dafeca7cd785
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-05-05 15:04:10 -0700

    Log, added to imports

--- laravel/app/Http/Controllers/InviteUserController.php
+++ laravel/app/Http/Controllers/InviteUserController.php
@@ -17,6 +17,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Str;
