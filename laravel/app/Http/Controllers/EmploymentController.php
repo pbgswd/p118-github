65a01e0b2459c2c825114427692997bc6cdf3377
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-05-13 18:25:09 -0700

    employment, manage expiry dates more globally. added deadline date update in constructor method.

--- laravel/app/Http/Controllers/EmploymentController.php
+++ laravel/app/Http/Controllers/EmploymentController.php
@@ -11,6 +11,14 @@ use Illuminate\View\View;
 
 class EmploymentController extends Controller
 {
+    public function __construct()
+    {
+        Employment::where('deadline', '<', now())
+            ->update(['status' => 0]);
+
+        Employment::where('deadline', '>', now())
+            ->update(['status' => 1]);
+    }
 
     /**
      * @return View
