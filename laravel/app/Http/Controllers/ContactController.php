294a1c7f5c77c136f056016958545031429c2b61
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-05-17 14:56:22 -0700

    google recaptcha updates and improvements, server side recaptcha validation working in dev now, able to push to prod.

--- laravel/app/Http/Controllers/ContactController.php
+++ laravel/app/Http/Controllers/ContactController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Session;
 use Illuminate\View\View;
+use ReCaptcha\ReCaptcha;
 
 class ContactController extends Controller
 {
@@ -38,17 +39,39 @@ class ContactController extends Controller
      */
     public function submit(SubmitContact $request): RedirectResponse
     {
-        usleep(250000);
+        define("RECAPTCHA_V3_SECRET_KEY", '6Ldv4sQaAAAAADrmuSc0lzoaf-AiVMMES6LxAt7g');
+
         $cc = [];
+
         if (config('app.env')  == 'local') {
             $cc = Options::testing_address_update_contacts();
         }
-// https://developers.google.com/recaptcha/docs/verify
-        // recaptcha secret: 6Ldv4sQaAAAAAJApVGt3T9XUyZcNFDrKLS_Umu1A
 
-        Log::debug('Contact page message  ' . $request['name'] . ' Sending to: ' .
+        Log::debug('Contact page message from ' . $request['name'] . ' sending to: ' .
             config('mail.office_admin.address') . ' ' . config('mail.office_admin.name') .', cc: ' .
             implode(", ", $cc) . ' at ' . date('Y-m-d H:i:s'));
+
+        $recaptcha = new ReCaptcha(RECAPTCHA_V3_SECRET_KEY);
+
+        $resp = $recaptcha->verify($request->input('g-recaptcha-response'), $request->ip());
+        Log::debug("Contact form Recaptcha v.3 score=".$resp->getScore());
+
+        if ($resp->isSuccess()) {
+            Log::debug("Contact form Recaptcha v.3 score=".$resp->getScore() . " submission  from ".
+                $request->name ." was a success.");
+        } else {
+            $errors = $resp->getErrorCodes();
+            Log::debug("Errors from Contact form Recaptcha v.3 from ". $request->name . ": ". serialize($errors));
+        }
+
+        Log::debug("Contact form submission data from ". $request->name . " " . serialize($resp));
+
+        if($resp->getScore() < 1) {
+            Log::debug('Contact form recaptcha score was low = '. $resp->getScore());
+            usleep(250000);
+        }
+
+        usleep(250000);
 
         Mail::send('emails.contact', ['data' => $request->all()], function ($m) use ($request, $cc) {
             $m->from( config('mail.from.address'), config('app.name') . 'Contact Page Message from '
