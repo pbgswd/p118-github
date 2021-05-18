291fa8777f409d837377420214240649fd38b0c8
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-05-17 18:36:52 -0700

    added in sitemap.xml generation here. Will be moved in to kernel and cron later.

--- laravel/app/Http/Controllers/SiteController.php
+++ laravel/app/Http/Controllers/SiteController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Tags\Url;
 
 class SiteController extends Controller
 {
@@ -13,6 +17,22 @@ class SiteController extends Controller
     public function index(): View
     {
         $user = Auth::user()->load('phone_number', 'user_info');
+
+        SitemapGenerator::create(config('app.url'))
+            ->getSitemap()
+            ->add(Url::create('/'))
+            ->add(Url::create('/pages'))
+            ->add(Url::create('/posts'))
+            ->add(Url::create('/topics'))
+            ->add(Url::create('/executive'))
+            ->add(Url::create('/bylaws'))
+            ->add(Url::create('/page/links'))
+            ->add(Url::create('/memoriams'))
+            ->add(Url::create('/agreements'))
+            ->add(Url::create('/organizations'))
+            ->add(Url::create('/venues'))
+            ->add(Url::create('/topic/contract-ratifications'))
+            ->writeToFile(public_path().'/sitemap.xml');
 
         return view('site', ['data' => ['user' => $user]]);
     }
