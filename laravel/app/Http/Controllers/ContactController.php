b245ccf61520a72dab58966f01dedc7789bb5337
Peter Gordon Linux Desktop <superwebdeveloper@gmail.com>
2021-05-19 01:03:51 -0700

    added session tracking and date comparisons to foil spammers in contact form submissions

--- laravel/app/Http/Controllers/ContactController.php
+++ laravel/app/Http/Controllers/ContactController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\Contact\SubmitContact;
 use App\Models\Contact;
 use App\Models\Options;
 use App\Models\Page;
+use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
@@ -22,7 +23,6 @@ class ContactController extends Controller
      */
     public function show(Contact $contact): View
     {
-        usleep(250000);
         $data = [];
 
         if (Auth::user()) {
@@ -40,6 +40,19 @@ class ContactController extends Controller
     public function submit(SubmitContact $request): RedirectResponse
     {
         define("RECAPTCHA_V3_SECRET_KEY", '6Ldv4sQaAAAAADrmuSc0lzoaf-AiVMMES6LxAt7g');
+        define("RECAPTCHA_THRESHOLD", "0.5");
+
+        $recaptcha = new ReCaptcha(RECAPTCHA_V3_SECRET_KEY);
+        $resp = $recaptcha->verify($request->input('g-recaptcha-response'), $request->ip());
+
+        Log::debug("\n" . "Contact form Recaptcha v.3 score=". $resp->getScore() . "\n");
+
+        $sub_time = $request->session()->get('submission_time');
+
+        if($request->session()->get('suspicious') == 'true' && $sub_time->diffInMinutes(Carbon::now()) < 5) {
+            Session::flash('error', 'Your submission appears suspicious.');
+            return redirect()->route('contact');
+        }
 
         $cc = [];
 
@@ -49,42 +62,42 @@ class ContactController extends Controller
 
         Log::debug('Contact page message from ' . $request['name'] . ' sending to: ' .
             config('mail.office_admin.address') . ' ' . config('mail.office_admin.name') .', cc: ' .
-            implode(", ", $cc) . ' at ' . date('Y-m-d H:i:s'));
-
-        $recaptcha = new ReCaptcha(RECAPTCHA_V3_SECRET_KEY);
-
-        $resp = $recaptcha->verify($request->input('g-recaptcha-response'), $request->ip());
-        Log::debug("Contact form Recaptcha v.3 score=".$resp->getScore());
+            implode(", ", $cc) . ' at ' . date('Y-m-d H:i:s'). "\n");
 
         if ($resp->isSuccess()) {
             Log::debug("Contact form Recaptcha v.3 score=".$resp->getScore() . " submission from ".
-                $request->name ." was returned as a success.");
+                $request->name ." was returned as a success. \n");
         } else {
             $errors = $resp->getErrorCodes();
-            Log::debug("Errors from Contact form Recaptcha v.3 from ". $request->name . ": ". serialize($errors));
+            Log::debug("Errors from Contact form Recaptcha v.3 from ". $request->name . ": ". serialize($errors). "\n");
         }
 
-        Log::debug("Contact form submission data from ". $request->name . " " . serialize($resp));
+        Log::debug("Contact form submission data from ". $request->name . " " . serialize($resp). "\n");
 
-        if($resp->getScore() < 1) {
-            Log::debug('Contact form recaptcha score was low = '. $resp->getScore());
-            usleep(250000);
+        if($resp->getScore() < RECAPTCHA_THRESHOLD) {
+            Log::debug('Contact form recaptcha score was low = '. $resp->getScore(). "\n");
+
+            $request->session()->put('submission_time', Carbon::now());
+            $request->session()->put('suspicious', 'true');
+
+            Session::flash('warning', 'Your message was rejected by the Recaptcha filter.
+                Please wait before trying again.');
+
+        } else {
+            Mail::send('emails.contact', ['data' => $request->all()], function ($m) use ($request, $cc) {
+                $m->from( config('mail.from.address'), config('app.name') . 'Contact Page Message from '
+                    . $request['name']);
+                $m->to(config('mail.office_admin.address'), config('mail.office_admin.name'));
+                if ($cc != '') {
+                    $m->cc($cc, $cc);
+                }
+                $m->replyTo($request['email'], $request['name']);
+                $m->subject('Contact Page '.$request['subject']);
+            });
+
+            Session::flash('success', 'Your message was sent.');
+            $request->session()->pull('suspicious');
         }
-
-        usleep(250000);
-
-        Mail::send('emails.contact', ['data' => $request->all()], function ($m) use ($request, $cc) {
-            $m->from( config('mail.from.address'), config('app.name') . 'Contact Page Message from '
-                . $request['name']);
-            $m->to(config('mail.office_admin.address'), config('mail.office_admin.name'));
-            if ($cc != '') {
-                $m->cc($cc, $cc);
-            }
-            $m->replyTo($request['email'], $request['name']);
-            $m->subject('Contact Page '.$request['subject']);
-        });
-
-        Session::flash('success', 'Your message was sent.');
 
         return redirect()->route('contact');
     }
