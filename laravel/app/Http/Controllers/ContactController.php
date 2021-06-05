46a2a49193e5024d8df2d891087d1220f9411b59
Vultr server, Peter Gordon <superwebdeveloper@gmail.com>
2021-06-05 19:22:50 +0000

    conact controller has issues, pulling down

--- laravel/app/Http/Controllers/ContactController.php
+++ laravel/app/Http/Controllers/ContactController.php
@@ -41,6 +41,12 @@ class ContactController extends Controller
      */
     public function submit(SubmitContact $request): RedirectResponse
     {
+/**
+        Log::debug("\n" . __METHOD__ .' hit, Contact page message submitted from ' . $request['name'] . ' sending to: ' .
+            config('mail.office_admin.address') . ' ' . config('mail.office_admin.name') .', cc: ' .
+            implode(", ", $cc) . ' at ' . date('Y-m-d H:i:s'). "\n");
+**/
+
         define("RECAPTCHA_V3_SECRET_KEY", '6Ldv4sQaAAAAADrmuSc0lzoaf-AiVMMES6LxAt7g');
         define("RECAPTCHA_THRESHOLD", "0.5");
 
