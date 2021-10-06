a96dcc9d4b3762ab2d3a791f332efa7199250b55
Peter Gordon Linux Desktop M2 Drive <superwebdeveloper@gmail.com>
2021-10-06 14:03:02 -0700

    js update of compilable assets

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -7,7 +6,7 @@
 
 require('./bootstrap');
 
-//window.Vue = require('vue');
+window.Vue = require('vue').default;
 
 /**
  * The following block of code may be used to automatically register your
@@ -17,18 +16,17 @@ require('./bootstrap');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-//.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-/**
+
 const app = new Vue({
-    el: '#app'
+    el: '#app',
 });
- **/
