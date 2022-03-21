9405dfa2e4bed43185b43bc6e4a845dcc4ad5258
Shift <shift@laravelshift.com>
2022-03-21 19:10:05 +0000

    Shift to class based routes

--- laravel/app/Providers/RouteServiceProvider.php
+++ laravel/app/Providers/RouteServiceProvider.php
@@ -28,15 +28,6 @@ class RouteServiceProvider extends ServiceProvider
 {
     public const HOME = '/site';
     /**
-     * This namespace is applied to your controller routes.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
-    // protected $namespace = 'App\Http\Controllers';
-
-    /**
      * Define your route model bindings, pattern filters, etc.
      *
      * @return void
@@ -121,7 +112,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapWebRoutes()
     {
         Route::middleware('web')
-             //->namespace($this->namespace)
+             //
              ->group(base_path('routes/web.php'));
     }
 
@@ -136,7 +127,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::prefix('api')
              ->middleware('api')
-             //->namespace($this->namespace)
+             //
              ->group(base_path('routes/api.php'));
     }
 
