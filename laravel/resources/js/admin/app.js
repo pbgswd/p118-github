64a2dceccc9a6557099ff536ba6b593e31efec4b
Peter Gordon Linux Desktop M2 Drive <superwebdeveloper@gmail.com>
2022-05-31 08:44:50 -0700

    git tidy up, files to check in

---
+++ laravel/resources/js/admin/app.js
@@ -0,0 +1,15 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import { createApp} from "vue";
+
+import ResendMembershipInvitation from "../components/admin/ResendMembershipInvitation";
+
+console.log('from inside app.js source');
+
+createApp({
+    components: {
+        ResendMembershipInvitation
+    }
+}).mount('#app');
