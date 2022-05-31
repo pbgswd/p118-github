419f3a66f9369e8ff0fd1e5987ff3a9474a5864b
Peter Gordon Linux Desktop M2 Drive <superwebdeveloper@gmail.com>
2022-05-30 23:55:46 -0700

    up to date factory for UserInfo

--- laravel/database/factories/UserInfoFactory.php
+++ laravel/database/factories/UserInfoFactory.php
@@ -23,8 +23,13 @@ class UserInfoFactory extends Factory
     {
         return [
             'user_id' => \App\Models\User::factory(),
+            'show_profile' => 1,
+            'show_picture' => 0,
             'share_email' => $this->faker->boolean,
             'share_phone' => $this->faker->boolean,
+            'file_name' => null,
+            'image' => null,
+            'about' => $this->faker->text(200),
         ];
     }
 }
