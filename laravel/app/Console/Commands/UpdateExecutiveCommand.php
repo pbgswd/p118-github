9bd284a183a80319123a3d12b6be8e3b606467bb
Peter Gordon Linux Desktop M2 Drive <superwebdeveloper@gmail.com>
2022-08-23 14:35:15 -0700

    deleting old tests that are redundant since shift tests, went through commands tests and only found one issue, marked for now as incomplete

--- laravel/app/Console/Commands/UpdateExecutiveCommand.php
+++ laravel/app/Console/Commands/UpdateExecutiveCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\Executive;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class UpdateExecutiveCommand extends Command
 {
@@ -39,6 +40,7 @@ class UpdateExecutiveCommand extends Command
      */
     public function handle()
     {
+
         $this->info('Update any expired executive status');
 
         Log::debug('running artisan command ' . __CLASS__);
