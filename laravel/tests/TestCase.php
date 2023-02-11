ad711b2a42ea6c7a27fa6ab315b16a6a356e29b3
Peter Gordon Linux Desktop M2 Drive <superwebdeveloper@gmail.com>
2023-02-11 15:32:00 -0800

    adding in log in to start of unit testing.

--- laravel/tests/TestCase.php
+++ laravel/tests/TestCase.php
@@ -24,6 +24,7 @@ abstract class TestCase extends BaseTestCase
     {
         parent::setUp();
         Log::debug('TestCase.php -- a test has started');
+
         $this->faker = Faker\Factory::create();
 
         $this->seed(AccessLevelConstantsSeeder::class);
