328f300207359a43594f88a9c42c21457111845f
Peter Gordon Linux Desktop M2 Drive <superwebdeveloper@gmail.com>
2024-01-12 20:24:11 -0800

    admin dev scratch page checkin

--- laravel/resources/views/admin/admin-development.blade.php
+++ laravel/resources/views/admin/admin-development.blade.php
@@ -9,6 +9,83 @@
     </div>
 </div>
 
+<div class="row mt-6 mb-6">
+    <div class="col-12">
+        <h1>some tailwind</h1>
+    </div>
+    <div class="col-12">
+
+
+
+
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+      <script>
+          body{
+              padding: 10rem;
+              background-color: #00283f;
+          }
+
+          #FileUpload.active {
+              // When files dragged over
+              // @apply shadow-outline-blue border-blue-300;
+              box-shadow: 0 0 0 3px rgba(164, 202, 254, 0.45);
+              border-color: #a4cafe;
+          }
+      </script>
+
+
+        <div class="flex flex-col flex-grow mb-3">
+            <div x-data="{ files: null }" id="FileUpload" class="block w-full py-2 px-3 relative bg-white appearance-none border-2 border-gray-300 border-solid rounded-md hover:shadow-outline-gray">
+                <input type="file" multiple
+                       class="absolute inset-0 z-50 m-0 p-0 w-full h-full outline-none opacity-0"
+                       x-on:change="files = $event.target.files; console.log($event.target.files);"
+                       x-on:dragover="$el.classList.add('active')" x-on:dragleave="$el.classList.remove('active')" x-on:drop="$el.classList.remove('active')"
+                >
+                <template x-if="files !== null">
+                    <div class="flex flex-col space-y-1">
+                        <template x-for="(_,index) in Array.from({ length: files.length })">
+                            <div class="flex flex-row items-center space-x-2">
+                                <template x-if="files[index].type.includes('audio/')"><i class="far fa-file-audio fa-fw"></i></template>
+                                <template x-if="files[index].type.includes('application/')"><i class="far fa-file-alt fa-fw"></i></template>
+                                <template x-if="files[index].type.includes('image/')"><i class="far fa-file-image fa-fw"></i></template>
+                                <template x-if="files[index].type.includes('video/')"><i class="far fa-file-video fa-fw"></i></template>
+                                <span class="font-medium text-gray-900" x-text="files[index].name">Uploading</span>
+                                <span class="text-xs self-end text-gray-500" x-text="filesize(files[index].size)">...</span>
+                            </div>
+                        </template>
+                    </div>
+                </template>
+                <template x-if="files === null">
+                    <div class="flex flex-col space-y-2 items-center justify-center">
+                        <i class="fas fa-cloud-upload-alt fa-3x text-currentColor"></i>
+                        <p class="text-gray-700">Drag your files here or click in this area.</p>
+                        <a href="javascript:void(0)" class="flex items-center mx-auto py-2 px-4 text-white text-center font-medium border border-transparent rounded-md outline-none bg-red-700">Select a file</a>
+                    </div>
+                </template>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+</div>
+
+
+<div class="row">
+
+
 <div class="mt-6 pt-6" id="anotherapp">
     <form method="post" action="">
         @CSRF
@@ -40,141 +117,35 @@
         </a>
 
         https://codepen.io/safrazik/pen/BaBpYme
-
-
-
     </div>
 </div>
-<div class="row border rounded mt-6 mb-3 p-3">
-<style>
-    #profile-pic-demo .drop-help-text {
-        display: none;
-    }
-    #profile-pic-demo .is-drag-over .drop-help-text {
-        display: block;
-    }
-    #profile-pic-demo .profile-pic-upload-block {
-        border: 2px dashed transparent;
-        padding: 20px;
-        padding-top: 0;
-    }
+    <div class="row border rounded border-2 mt-6 mb-3 p-3">
+        <h1>Here  is some Vue stuff</h1> <br />
+        <br />
+        <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
-    #profile-pic-demo .is-drag-over.profile-pic-upload-block {
-        border-color: #AAA;
-    }
-    #profile-pic-demo .vue-file-agent {
-        width: 180px;
-        float: left;
-        margin: 0 15px 5px 0;
-        border: 0;
-        box-shadow: none;
-    }
+        <script>
+            import { createApp } from 'vue'
 
+            createApp({
+                data() {
+                    return {
+                        count: 0
+                    }
+                }
+            }).mount('#app')
 
-</style>
+            export default {
+                props: ['count'],
+                // Other Vue component options
+            };
+
+        </script>
+       ## <div id="app" v-pre:"count"></div>##
 
 
-    <script type="text/x-template" id="vue-file-agent-demo">
-        <div id="profile-pic-demo" class="bg-light pt-3">
-            <VueFileAgent
-                class="profile-pic-upload-block"
-                ref="profilePicRef"
-                :multiple="false"
-                :deletable="false"
-                :meta="false"
-                :compact="true"
-                :accept="'image/*'"
-                :helpText="'Drag an image file here'"
-                :errorText="{
-        type: 'Please select an image',
-      }"
-                v-model="profilePic"
-                @select="onSelect($event)"
-            >
-                <template v-slot:before-outer>
-                </template >
-                <template v-slot:after-inner>
-                    <span title="after-inner" class="btn btn-link btn-sm btn-block">Select image file</span>
-                </template >
-                <template v-slot:after-outer>
-                    <div title="after-outer">
-                        <p>Please select an image and click the upload button</p>
-                        <div class="drop-help-text">
-                            <p class="text-success">Drop the file!</p>
-                        </div>
-                        <button type="button" class="btn btn-primary" :class="{'disabled': uploaded || !profilePic}" @click="upload()">Upload</button>
-                        <button type="button" class="btn" :class="[uploaded ? 'btn-danger' : 'btn-secondary']" v-if="profilePic" @click="removePic()">Remove</button>
-                        <div class="clearfix"></div>
-                    </div>
-                </template >
-            </VueFileAgent>
-        </div>
-    </script>
-
-<script>
-
-    var component = {
-        data: function(){
-            return {
-                name: 'Gapal',
-                profilePic: null,
-                uploaded: false,
-                uploadUrl: 'https://www.mocky.io/v2/5d4fb20b3000005c111099e3',
-                uploadHeaders: {},
-            }
-        },
-        methods: {
-            removePic: function(){
-                var profilePic = this.profilePic;
-                this.$refs.profilePicRef.deleteUpload(this.uploadUrl, this.uploadHeaders, [profilePic]);
-                this.profilePic = null;
-                this.uploaded = false;
-            },
-            upload: function(){
-                var self = this;
-                this.$refs.profilePicRef.upload(this.uploadUrl, this.uploadHeaders, [this.profilePic]).then(function(){
-                    self.uploaded = true;
-                    setTimeout(function(){
-                        // self.profilePic.progress(0);
-                    }, 500);
-                });
-            },
-            onSelect: function(fileRecords){
-                console.log('onSelect', fileRecords);
-                this.uploaded = false;
-            }
-        }
-    }
-
-    component.template = '#vue-file-agent-demo';
-
-    Vue.component('vue-file-agent-demo', component);
-
-    new Vue({
-        el: '#app'
-    });
-</script>
-
-    <!-- ----------------------------- -->
-
-    <div class="container py-3">
-        <div id="app">
-
-            <h5><a target="_blank" href="https://safrazik.github.io/vue-file-agent">Vue File Agent</a> Playground</h5>
-
-            <hr>
-
-            <vue-file-agent-demo></vue-file-agent-demo>
-
-        </div>
     </div>
-</div>
-<div class="row border rounded mt-6 mb-3 p-3">
-    <div class="col-12">
-        <h2>Todo image library, insert into content</h2>
-        <a href="https://madewithvuejs.com/laravel-media-manager">
-            https://madewithvuejs.com/laravel-media-manager
-        </a>
-    </div>
+
+
 </div>
 @endsection
