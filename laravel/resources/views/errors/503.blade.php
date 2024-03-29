e4c062823646976b97ed8a4ac50f3e9b98ea9e91
Peter Gordon Linux Desktop M2 Drive <superwebdeveloper@gmail.com>
2024-03-28 23:29:49 -0700

    Cleaned up error pages a tiny bit. Layout had issues, now its a bit more civil now.

--- laravel/resources/views/errors/503.blade.php
+++ laravel/resources/views/errors/503.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('Service Unavailable - This site is down temporarily if you see this message.'))
 @section('code', '503')
-@section('message', __('Service Unavailable - contact admin for access.'))
+@section('message', __('The website service has been paused temporarily. Check back in a few minutes.'))
