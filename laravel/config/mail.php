a8e89f6404e35cf8dc6862555ed3ea3f53cb4aa2
Shift <shift@laravelshift.com>
2024-06-22 18:15:32 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files easier, Shift defaulted them and merged your true customizations - where ENV variables may not be used.

--- laravel/config/mail.php
+++ laravel/config/mail.php
@@ -34,14 +34,15 @@ return [
     | sending an e-mail. You will specify which one you are using for your
     | mailers below. You are free to add additional mailers as required.
     |
-    | Supported: "smtp", "sendmail", "mailgun", "ses",
-    |            "postmark", "log", "array", "failover"
+    | Supported: "smtp", "sendmail", "mailgun", "ses", "ses-v2",
+    |            "postmark", "log", "array", "failover", "roundrobin"
     |
     */
 
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
+            'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
@@ -49,18 +50,26 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'verify_peer' => env('MAIL_VERIFY_PEER'),
+            'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
 
         'ses' => [
             'transport' => 'ses',
         ],
 
-        'mailgun' => [
-            'transport' => 'mailgun',
-        ],
-
         'postmark' => [
             'transport' => 'postmark',
+            // 'message_stream_id' => null,
+            // 'client' => [
+            //     'timeout' => 5,
+            // ],
+        ],
+
+        'mailgun' => [
+            'transport' => 'mailgun',
+            // 'client' => [
+            //     'timeout' => 5,
+            // ],
         ],
 
         'sendmail' => [
@@ -84,6 +93,14 @@ return [
                 'log',
             ],
         ],
+
+        'roundrobin' => [
+            'transport' => 'roundrobin',
+            'mailers' => [
+                'ses',
+                'postmark',
+            ],
+        ],
     ],
 
     /*
@@ -98,8 +115,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', env('MAIL_FROM_ADDRESS')),
-        'name' => env('MAIL_FROM_NAME', env('MAIL_FROM_NAME')),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     'admin' => [
@@ -111,8 +128,6 @@ return [
         'address' => env('MAIL_OFFICE_EMAIL_RECIPIENT'),
         'name' => env('MAIL_OFFICE_EMAIL_NAME'),
     ],
-
-    //todo define reply_to  //'reply_to' => ['address' => '', 'name' => ''];
 
     'encryption' => env('MAIL_ENCRYPTION', 'tls'),
 
