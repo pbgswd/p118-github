8d1c10148de15f6ca6c06ad6df786296ce4d7aee
Peter Gordon Linux Desktop M2 Drive <superwebdeveloper@gmail.com>
2024-07-12 14:50:54 -0700

    committee page layout cleanup

--- laravel/resources/views/committee.blade.php
+++ laravel/resources/views/committee.blade.php
@@ -10,10 +10,10 @@
                 </h6>
             </div>
             @can(['manage committee'])
-                <div class="col-12 col-md-6 text-md-right">
+                <div class="col-12 col-md-6 text-end">
                     <a href="{{route('committee_edit', $data['committee']->slug)}}"
                        title="Edit {{$data['committee']->name}}">
-                        <i class="fas fa-edit"></i> Edit
+                        <i class="fas fa-edit"></i> Admin Edit
                     </a>
                 </div>
             @endcan
@@ -36,33 +36,39 @@
                 </h4>
             </div>
         </div>
-        <div class="row mt-3 mb-3">
-            <div class="col-12 border border-dark rounded pt-2 pb-2">
-                <h5>{{$data['committee']->name}} Executive</h5>
-                <ul class="list-group">
-                    @forelse ($data['executives'] as $exec)
+        <div class="row my-3 border border-dark rounded p-2 pb-4 text-center d-flex justify-content-center">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center">
+                    <h4>{{$data['committee']->name}} Executive</h4>
+                </div>
+                <div class="col-12 d-flex justify-content-center">
+                    <ul class="list-group list-group-horizontal-lg">
+                        @forelse ($data['executives'] as $exec)
+                            <li class="list-group-item">
+                                <i class="fas fa-user-tie"></i>
+                                {{$exec->pivot->role}}:
+                                <a href="{{route('member', $exec->id)}}">
+                                    {{$exec->name}}
+                                </a>
+                            </li>
+                        @empty
+                            <li class="list-group-item">
+                                No Executives defined
+                            </li>
+                        @endforelse
                         <li class="list-group-item">
-                            <i class="fas fa-user-tie"></i>
-                            {{$exec->pivot->role}}:
-                            <a href="{{route('member', $exec->id)}}">
-                                {{$exec->name}}
+                            <i class="far fa-envelope"></i>
+                            Email:
+                            <a href="mailto:{{$data['committee']->email}}?subject={{$data['committee']->name}} committee">
+                                {{$data['committee']->email}}
                             </a>
                         </li>
-                    @empty
-                        <li class="list-group-item">
-                            No Executives defined
-                        </li>
-                    @endforelse
-                    <li class="list-group-item">
-                        <i class="far fa-envelope"></i>
-                        Email:
-                        <a href="mailto:{{$data['committee']->email}}?subject={{$data['committee']->name}} committee">
-                            {{$data['committee']->email}}
-                        </a>
-                    </li>
-                </ul>
-                <h5 class="mt-2">All Current Members of {{$data['committee']->name}}</h5>
-                <ul class="list-group">
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <h4 class="mt-3 text-center">Current Members</h4>
+                <ul class="list-group list-group-horizontal-lg">
                     @forelse($data['committee']->active_committee_members as $mbr)
                         <li class="list-group-item">
                             @if($mbr->user_info->show_profile == 1)
@@ -81,7 +87,8 @@
                 </ul>
             </div>
         </div>
-        <div class="row mb-4">
+
+        <div class="row mb-2">
             <div class="col-12 col-md-6">
                 <h4>
                     <i class="far fa-newspaper"></i>
@@ -100,46 +107,40 @@
                 @endif
             </div>
         </div>
-        <div class="row border rounded p-2 mb-3 bg-light">
-            <div class="col-12 m-2 text-secondary">
-                <h4>Sticky Posts</h4>
-            </div>
+        <div class="row mb-3">
             <div class="col-12 {{ $data['sticky_posts']->count() % 2 > 0 ? 'col-md-12' : 'col-md-6' }}">
                 @forelse($data['sticky_posts'] as $sp)
-                    <div class="border border-dark rounded p-4 mb-2 bg-light">
+                    <div class="border border-dark rounded p-2 mb-1 bg-light">
+                        <h6 class="text-body-tertiary">Sticky Post</h6>
                         <h4>
                             <a href="{{route('public_committee_post_show', [$data['committee']->slug, $sp->slug])}}"
                                title="{{$sp->title}}">
                                 {{$sp->title}}
                             </a>
                         </h4>
-                        Posted by: {{$sp->creator->name}}
                         {{ \Carbon\Carbon::parse($sp->updated_at)->format(' F j, Y') }}
                     </div>
                 @empty
                 @endforelse
             </div>
         </div>
-
         <div class="row">
-            <div class="col-12 border border-dark rounded mt-1 p-4">
                 @forelse($data['posts'] as $p)
-                    <h3>
-                        <a href="{{route('public_committee_post_show', [$data['committee']->slug, $p->slug])}}"
-                           title="{{$p->title}}">
-                            {{$p->title}}
-                        </a>
-                    </h3>
-                    Posted by: {{$p->creator->name}}
-                    {{ \Carbon\Carbon::parse($p->updated_at)->format(' F j, Y') }}
+                    <div class="col-12 p-2 mb-2">
+                        <h3>
+                            <a href="{{route('public_committee_post_show', [$data['committee']->slug, $p->slug])}}"
+                               title="{{$p->title}}">
+                                {{$p->title}}
+                            </a>
+                        </h3>
+                        {{ \Carbon\Carbon::parse($p->updated_at)->format(' F j, Y') }}
+                    </div>
                 @empty
                     <p class="text-secondary">
                         No posts yet, but there will be soon!
                     </p>
                 @endforelse
-            </div>
         </div>
-
         <div class="row">
             <div class="col-12">
                 @if($data['posts']->count() > 5)
