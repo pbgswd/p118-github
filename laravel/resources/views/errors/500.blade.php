8721ab9d35fb22b27151036d6fe432e057aa5282
Peter Gordon Linux Desktop M2 Drive <superwebdeveloper@gmail.com>
2024-08-05 19:59:11 -0700

    added email to error 500 page

--- laravel/resources/views/errors/500.blade.php
+++ laravel/resources/views/errors/500.blade.php
@@ -2,4 +2,6 @@
 
 @section('title', __('Server Error'))
 @section('code', '500')
-@section('message', __("Server Error. Something isn't right. Maybe let the Maintainer know about it."))
+@section('message', __("Server Error. Something isn't right. Maybe let the Maintainer know about it.
+    You can send a descriptive message about what you were trying to do to
+    <a href='mailto:webster@iatse118.com'>webster@iatse118.com</a>"))
