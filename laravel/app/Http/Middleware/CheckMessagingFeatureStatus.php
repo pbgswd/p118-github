663a3ffaad883ebc5974e3f8e3dcd27f893bc162
Peter Gordon Linux Desktop M2 Drive <superwebdeveloper@gmail.com>
2024-08-08 21:39:00 -0700

    app/Http/Middleware/CheckMessagingFeatureStatus.php crashes in local when no data is in messages table, I think... Using more general return hint in signature, Response

--- laravel/app/Http/Middleware/CheckMessagingFeatureStatus.php
+++ laravel/app/Http/Middleware/CheckMessagingFeatureStatus.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class CheckMessagingFeatureStatus
 {
-    public function handle(Request $request, Closure $next): RedirectResponse
+    public function handle(Request $request, Closure $next): \Symfony\Component\HttpFoundation\Response // changed from RedirectResponse
     {
         if (env('ENABLE_MESSAGING_FEATURE') == 0) {
             return redirect('/');
