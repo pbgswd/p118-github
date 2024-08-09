9ce863af00371e2b30e88dabf4bd7cd17b5b2168
Peter Gordon Linux Desktop M2 Drive <superwebdeveloper@gmail.com>
2024-08-08 21:44:03 -0700

    checking in update permissions script to keep bootstrap and storage running true

---
+++ update_permissions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# bash script to correct permissions and ownership of bootstrap/cache and storage
+# change permissions for this file to run: chmod +x update_permissions.sh
+# run as ./update_permissions.sh after composer update, composer install, getting error 500s
+
+cd laravel
+# Check if cd was successful
+if [ $? -eq 0 ]; then
+  echo "Successfully changed directory to $(pwd)"
+else
+  echo "Failed to change directory"
+  exit 1
+fi
+
+sudo chown -R www-data:www-data bootstrap/cache
+sudo chmod -R 775 bootstrap/cache
+
+sudo chown -R www-data:www-data storage
+sudo find storage -type f -exec chmod 644 {} \;
+sudo find storage -type d -exec chmod 755 {} \;
+
