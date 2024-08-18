2ca5adb168678cfff3c921ef98ef75a11bd7ba1e
Peter Gordon Linux Desktop M2 Drive <superwebdeveloper@gmail.com>
2024-08-18 15:51:49 -0700

    handle add create method for previous commit, changes to blade

--- laravel/resources/views/admin/committee_manage_membership.blade.php
+++ laravel/resources/views/admin/committee_manage_membership.blade.php
@@ -25,60 +25,63 @@
                                 role for {{$data['user']->name}} in {{$data['committee']->name}}
                             </label>
                         </h4>
-                                  <select class="form-select" name="role" aria-label="Default select example">
-                                    @foreach($data['committee_roles']['committee_roles'] as $k => $v)
-                                        @if($data['user']->committee_memberships[0]->pivot->role == $v)
-                                            <option value="{{$v}}" selected>{{$k}}</option>
-                                        @else
-                                            <option value="{{$v}}">{{$k}}</option>
-                                        @endif
-                                    @endforeach
-                                </select>
-
-                            <br />
-                            @if($data['action'] == 'Edit')
-                                <i class="far fa-edit"></i>
-                            @else
-                                <i class="fas fa-user-plus"></i>
+                        <select class="form-select" name="role" aria-label="Default select example">
+                            @if($data['action'] == 'Add')
+                                <option value="" selected>Select a Committee role</option>
                             @endif
-                            <button type="submit" class="btn btn-secondary">
-                                {{$data['action']}}
-                            </button>
+                            @foreach($data['committee_roles']['committee_roles'] as $k => $v)
+                              @if(isset($data['user']->committee_memberships[0]->pivot->role) && $data['user']->committee_memberships[0]->pivot->role == $v)
+                                  <option value="{{$v}}" selected>{{$k}}</option>
+                              @else
+                                  <option value="{{$v}}">{{$k}}</option>
+                              @endif
+                            @endforeach
+                        </select>
 
-                    </div>
-                </div>
-            </form>
-        </div>
+<br />
+@if($data['action'] == 'Edit')
+<i class="far fa-edit"></i>
+@else
+<i class="fas fa-user-plus"></i>
+@endif
+<button type="submit" class="btn btn-secondary">
+{{$data['action']}}
+</button>
 
-            @if($data['action'] == 'Edit')
-                <div class="row mt-6">
-                    <div class="col-12 p-3 mt-6">
-                        <form method="post" name="manage_committee_members"
-                              action="{{ route('admin_delete-committee_member',
-                                        [$data['committee']->slug, $data['user']->id]) }}"
-                              enctype="multipart/form-data"
-                              class="needs-validation" novalidate>
-                            {!! csrf_field() !!}
-                            {!! method_field('DELETE') !!}
-                            <div class="form-group">
-                                <h4 class="mt-4 mb-3">
-                                    Delete  {{$data['user']->name}} from {{$data['committee']->name}}
-                                </h4>
-                                <input type="hidden" name="user_id" value="{{$data['user']->id}}" />
-                                <i class="far fa-trash-alt fa-2x"></i>
-                                <input class="btn btn-outline-danger" type="submit" value="Delete" />
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            @endif
+</div>
+</div>
+</form>
+</div>
 
-    <div class="row mt-lg-5">
-        <div class="col-md-12">
-            <a href="{{route('admin-list-committee-members', $data['committee']->slug)}}">
-                <i class="far fa-hand-point-left fa-2x"></i> Go back
-            </a>
-        </div>
-    </div>
+@if($data['action'] == 'Edit')
+<div class="row mt-6">
+<div class="col-12 p-3 mt-6">
+<form method="post" name="manage_committee_members"
+action="{{ route('admin_delete-committee_member',
+      [$data['committee']->slug, $data['user']->id]) }}"
+enctype="multipart/form-data"
+class="needs-validation" novalidate>
+{!! csrf_field() !!}
+{!! method_field('DELETE') !!}
+<div class="form-group">
+<h4 class="mt-4 mb-3">
+  Delete  {{$data['user']->name}} from {{$data['committee']->name}}
+</h4>
+<input type="hidden" name="user_id" value="{{$data['user']->id}}" />
+<i class="far fa-trash-alt fa-2x"></i>
+<input class="btn btn-outline-danger" type="submit" value="Delete" />
+</div>
+</form>
+</div>
+</div>
+@endif
+
+<div class="row mt-lg-5">
+<div class="col-md-12">
+<a href="{{route('admin-list-committee-members', $data['committee']->slug)}}">
+<i class="far fa-hand-point-left fa-2x"></i> Go back
+</a>
+</div>
+</div>
 </div>
 @endsection
