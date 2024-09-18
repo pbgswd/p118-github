98bb0c10143da1d6002ed3f10d8f13ab387f3ab0
Shift <shift@laravelshift.com>
2024-09-18 19:16:38 +0000

    Shift cleanup

--- laravel/tests/CreatesApplication.php
+++ laravel/tests/CreatesApplication.php
@@ -2,13 +2,10 @@
 
 namespace Tests;
 
-use Illuminate\Contracts\Console\Kernel;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Artisan;
 
 trait CreatesApplication
 {
-
     private function clearCache(): void
     {
         Artisan::call('cache:clear');
