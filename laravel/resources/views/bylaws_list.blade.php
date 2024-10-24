fde2750593a540cb2ad09c117b262d4233032f0f
Peter Gordon Linux Desktop M2 Drive <superwebdeveloper@gmail.com>
2024-10-23 20:56:46 -0700

    does not need title, line one, in blade, created in the controller.

--- laravel/resources/views/bylaws_list.blade.php
+++ laravel/resources/views/bylaws_list.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.jumbo',  ['title' => '<i class="fas fa-list"></i> bylaw Postings'])
+@extends('layouts.jumbo')
 @section('content')
 <div class="container border border-dark rounded mt-3 mb-3" style="background: rgba(220,220,220,0.8);">
     <div class="row p-2">
