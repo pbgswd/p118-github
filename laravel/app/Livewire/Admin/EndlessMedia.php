2b49f8b6fcc404e99331a0bc1f1ceb8772a92c6d
Peter Gordon Linux Desktop M2 Drive <superwebdeveloper@gmail.com>
2024-11-13 16:54:40 -0800

    endless media work in progress, checking in to share on other instance

--- laravel/app/Livewire/Admin/EndlessMedia.php
+++ laravel/app/Livewire/Admin/EndlessMedia.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Admin;
 
 use App\Models\Attachment;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\View\View;
 use Livewire\Component;
@@ -10,19 +11,22 @@ use Livewire\Component;
 class EndlessMedia extends Component
 {
 
-    public $perPage = 10;
+    public $perPage = 1000;
 
     public function render(): View
     {
-        $attachments = Attachment::with('user')->where('file_type', 'image')
-            ->orderBy('id', 'ASC')->paginate($this->perPage);
+        $attachments = Attachment::query()->with('user')->where('file_type', 'image')
+            ->with(['venue' => function ($query) {
+                $query->select('file_name', 'image AS file', DB::raw("'venues' as subfolder"))->whereNotNull('image');
+            }])
+            ->paginate($this->perPage);
         $attachments->each(function ($item) {
             //todo have a thumb size for every image asset
             $item->file_size =
                 round(File::size('storage/'.$item->subfolder.'/'.$item->file) /
                     1024, 2);
         });
-
+dd($attachments);
         $data['attachments'] = $attachments;
 
         return view('livewire.admin.endless-media', ['data' => $data]);
