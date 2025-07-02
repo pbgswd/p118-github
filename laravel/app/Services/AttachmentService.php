6a68da34540efed1cf94ebd82eed187491a72a79
Peter Gordon - peter-system-ubuntu-desktop-m2 <superwebdeveloper@gmail.com>
2025-07-01 20:42:53 -0700

    Change in downloadAttachment method in AttachmentService, syntax for L12. Killed downloads.  See Line 76

--- laravel/app/Services/AttachmentService.php
+++ laravel/app/Services/AttachmentService.php
@@ -25,14 +25,11 @@ class AttachmentService
             $attachment->save();
             $model->attachments()->attach($attachment);
         }
-
         return true;
     }
 
     public function updateAttachment(Request $request, HasAttachment $model): bool
     {
-        // dd([$request->files, $model]);
-
         if (isset($request->attachment)) {
             foreach ($request->attachment as $k => $v) {
                 $attachment = Attachment::find($k);
@@ -44,12 +41,9 @@ class AttachmentService
                         Storage::disk($model->getAttachmentFolder())->delete($attachment['file']);
                         Attachment::destroy($v['id']);
                     }
-
                     continue;
                 }
-
                 $keys = array_keys($request->attachment);
-
                 foreach ($keys as $k) {
                     if ($attachment->id == $k) {
                         $attachment->access_level = $request->attachment[$k]['access_level'] ?? $model->getAttachmentAccessLevel();
@@ -58,22 +52,18 @@ class AttachmentService
                     }
                 }
             }
-
             return true;
         }
-
         return false;
     }
 
     public function destroyAttachments(HasAttachment $model): bool
     {
         $model->load('attachments');
-
         foreach ($model->attachments as $attachment) {
             Storage::disk($model->getAttachmentFolder())->delete($attachment['file']);
             Attachment::destroy($attachment['id']);
         }
-
         return true;
     }
 
@@ -81,23 +71,21 @@ class AttachmentService
     {
         if (Auth::check() === false && $attachment->access_level != AccessLevelConstants::PUBLIC) {
             Session::flash('error', 'Please log in first and try the download link again.');
-
             return redirect()->route('login');
         } else {
-            return Storage::download($folder.'/'.$attachment['file'],
-                $attachment['file_name'], ['Content-Disposition' => 'inline; filename="'.$attachment['file_name'].'"']);
+            return Storage::disk($folder)->download($attachment['file'],
+                $attachment['file_name'],
+                ['Content-Disposition' => 'inline; filename="'.$attachment['file_name'].'"']);
         }
     }
 
     public static function human_filesize($bytes, int $decimals = 2): string
     {
         $factor = \floor((\strlen($bytes) - 1) / 3);
-
         $sz = '';
         if ($factor > 0) {
             $sz = 'KMGT';
         }
-
         return \sprintf("%.{$decimals}f", $bytes / (1024 ** $factor)).@$sz[$factor - 1].'B';
     }
 }
